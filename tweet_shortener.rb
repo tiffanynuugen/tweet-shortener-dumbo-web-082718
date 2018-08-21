7b8e817f898d42eec08aa7c7f3cc9e42fc58afa3
Tiffany Nguyen <tiffanynuugen@gmail.com>
2018-08-21 04:29:42 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -9,27 +9,31 @@ def dictionary
 "and" => "&"
     }
 end
+
 def word_substituter(tweet)
-    tweet.split.collect do |word|
-    keys = dictionary.keys.flatten.join(",").split(",")
-    low_case = word.downcase
-        if keys.include?(low_case) && low_case == "for" || low_case == "four"
-            word = dictionary[["for, four"]]
-        elsif keys.include?(low_case) && low_case == "to" || low_case == "two" || low_case == "too"
-            word = dictionary[["to, two, too"]]
-        elsif keys.include?(low_case)
-            word = dictionary[low_case]
-        else
-            word
-        end
+  tweet.split.collect do |word|
+  keys = dictionary.keys.flatten.join(",").split(",")
+  low_case = word.downcase
+if keys.include?(low_case) && low_case == "for" || low_case == "four"
+  word = dictionary[["for, four"]]
+elsif keys.include?(low_case) && low_case == "to" || low_case == "two" || low_case == "too"
+  word = dictionary[["to, two, too"]]
+elsif keys.include?(low_case)
+  word = dictionary[low_case]
+else
+  word
+end
   end.join(" ")
 end
- def bulk_tweet_shortener(array)
-  array.each{|each_array| puts word_substituter(each_array)}
+
+def bulk_tweet_shortener(array)
+ array.each{|each_array| puts word_substituter(each_array)}
 end
- def selective_tweet_shortener(tweet)
-  tweet.length > 140 ? word_substituter(tweet) : tweet
+
+def selective_tweet_shortener(tweet)
+ tweet.length > 140 ? word_substituter(tweet) : tweet
 end
- def shortened_tweet_truncator(tweet)
-  selective_tweet_shortener(tweet).length > 140 ? tweet[0..139] : selective_tweet_shortener(tweet)
+
+def shortened_tweet_truncator(tweet)
+ selective_tweet_shortener(tweet).length > 140 ? tweet[0..139] : selective_tweet_shortener(tweet)
 end
