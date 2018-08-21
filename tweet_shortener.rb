8f891df1620815945d0826d88a260cb1bb8c8eae
Tiffany Nguyen <tiffanynuugen@gmail.com>
2018-08-21 04:14:52 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,49 +1,29 @@
-def word_substituter(tweet)
-  words = tweet.split(" ")
-  short_tweet = []
-
-  words.each do |word|
-    case word.downcase
-    when "hello"
-      short_tweet << "hi"
-    when "to", "two", "too"
-      short_tweet << "2"
-    when "for", "four"
-      short_tweet << "4"
-    when "be"
-      short_tweet << "b"
-    when "you"
-      short_tweet << "u"
-    when "at"
-      short_tweet << "@"
-    when "and"
-      short_tweet << "&"
-    else
-      short_tweet << word
-    end
+def shorten_tweet(tweet, replacements)
+  if tweet.length <= 140
+    return tweet
   end
-  short_tweet.join(" ")
+
+  new_tweet = []
+  tweet.split().each do |word|
+    new_tweet << (replacements[word.downcase] ? replacements[word.downcase] : word)
+  end
+
+  new_tweet.join(' ')[0, 140]
 end
 
-def bulk_tweet_shortener(tweets)
-  tweets.each do |tweet|
-    tweet = word_substituter(tweet)
-    puts "#{tweet}"
+def shorten_tweet2(tweet, replacements)
+  if tweet.length <= 140
+    return tweet
   end
+
+  replacements.keys.each do |key|
+    mod_key = key.downcase.center(key.length + 2)
+    tweet.gsub! mod_key, ' ' + replacements[key] + ' '
+  end
+  tweet[0, 140]
 end
 
-def selective_tweet_shortener(tweet)
-  if tweet.length > 140
-    word_substituter(tweet)
-  else
-    tweet
-  end
+tweets.each do |tweet|
+  puts shorten_tweet(tweet, replacements)
+  puts shorten_tweet2(tweet, replacements)
 end
-
-def shortened_tweet_truncator(tweet)
-  tweet = selective_tweet_shortener(tweet)
-  if tweet.length > 140
-    tweet[0..136] + "..."
-  else
-    tweet
-  end
